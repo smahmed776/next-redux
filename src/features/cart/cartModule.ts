aedd85055d38d1969471d730aac5e6bf3bf98e6f
Syed Mahbub <xsmahmed776@gmail.com>
2022-02-23 20:41:22 +0600

    remove unwanted commit task-2

--- src/features/cart/cartModule.ts
+++ src/features/cart/cartModule.ts
@@ -1,4 +1,3 @@
-
 interface CartItem {
   id: number;
   title: string;
@@ -23,29 +22,29 @@ const discountCodes: Array<DiscountCode> = [
   {
     code: "halfDiscount",
     type: "cart",
-    value: 50
+    value: 50,
   },
   {
     code: "fullDiscount",
     type: "cart",
-    value: 100
+    value: 100,
   },
   {
     code: "tfiveDiscount",
     type: "item",
     value: 25,
-    itemId: 3
+    itemId: 3,
   },
 ];
 
 const cart: Cart = {
   items: [],
-  discount: []
+  discount: [],
 };
 
 const addItem = (product: CartItem): void => {
-  const isExist = cart.items.find(item => item.id === product.id);
-  if(isExist){
+  const isExist = cart.items.find((item) => item.id === product.id);
+  if (isExist) {
     isExist.quantity++;
     console.log("Quantity increamented for this product", isExist);
   } else {
@@ -57,21 +56,19 @@ const addItem = (product: CartItem): void => {
   }
 };
 
-
 const removeItem = (id: number): void => {
-  const isExist = cart.items.find(item => item.id === id && item.quantity >= 1)
+  const isExist = cart.items.find(
+    (item) => item.id === id && item.quantity >= 1
+  );
 
-  if(isExist.quantity > 1) {
+  if (isExist.quantity > 1) {
     isExist.quantity--;
-    console.log(
-      "item quantity decreased !",
-      isExist
-      );
-    } else if (isExist.quantity === 1) {
-    cart.items = cart.items.filter(item => item.id !== isExist.id)
-    console.log("Item removed from cart!")
-  }  else {
-    console.log("item does not available in cart")
+    console.log("item quantity decreased !", isExist);
+  } else if (isExist.quantity === 1) {
+    cart.items = cart.items.filter((item) => item.id !== isExist.id);
+    console.log("Item removed from cart!");
+  } else {
+    console.log("item does not available in cart");
   }
 };
 
@@ -92,7 +89,9 @@ const addDiscount = (discountCode: string): void => {
       }
     } else {
       const product =
-        cart.items[cart.items.findIndex((item) => item.id === checkCode.itemId)];
+        cart.items[
+          cart.items.findIndex((item) => item.id === checkCode.itemId)
+        ];
       // check if already applied this code before
       const isApplied = product.discountApplied.includes(checkCode.code);
       if (!isApplied) {
@@ -118,34 +117,38 @@ const getCartItemPrice = (item: CartItem): number => {
     const codeValue = discountCodes.filter((code) =>
       item.discountApplied.includes(code.code)
     );
-    console.log(codeValue);
     const totalDiscount = codeValue.reduce(
       (total, item) => total + item.value,
       0
     );
-    console.log(totalDiscount);
-    totalPrice =(item.price * item.quantity) - ((item.price * item.quantity * totalDiscount) / 100);
+    totalPrice =
+      item.price * item.quantity -
+      (item.price * item.quantity * totalDiscount) / 100;
   } else {
     totalPrice = item.quantity * item.price;
   }
   return totalPrice;
 };
 
-
 // get total price of product store
 
 const getTotalPrice = (): number => {
-  // check if any item has discounted price 
-  const haveDiscount = cart.items.filter(item => item.discountApplied.length > 0);
-  let totalDiscountedItemPrice : number = 0;
-  if(haveDiscount){
-    const discountedItemPrice : number[] = [];
-    // get discounted price of these items 
-    haveDiscount.forEach(item => {
-     const price =  getCartItemPrice(item);
-     discountedItemPrice.push(price)
-    })
-    totalDiscountedItemPrice = discountedItemPrice.reduce((total, item) => total + item, 0)
+  // check if any item has discounted price
+  const haveDiscount = cart.items.filter(
+    (item) => item.discountApplied.length > 0
+  );
+  let totalDiscountedItemPrice: number = 0;
+  if (haveDiscount) {
+    const discountedItemPrice: number[] = [];
+    // get discounted price of these items
+    haveDiscount.forEach((item) => {
+      const price = getCartItemPrice(item);
+      discountedItemPrice.push(price);
+    });
+    totalDiscountedItemPrice = discountedItemPrice.reduce(
+      (total, item) => total + item,
+      0
+    );
   }
   const codeApplied = cart.discount.length > 0;
   let totalPrice = 0;
@@ -158,36 +161,46 @@ const getTotalPrice = (): number => {
       0
     );
     const cartPrice = cart.items.reduce((total, item) => {
-      if(item.discountApplied.length === 0){
-        return total + item.quantity * item.price
+      if (item.discountApplied.length === 0) {
+        return total + item.quantity * item.price;
       } else {
-        return total
+        return total;
       }
     }, 0);
     const finalPrice = cartPrice + totalDiscountedItemPrice;
 
-    totalPrice = finalPrice - (finalPrice * totalDiscount / 100);
+    totalPrice = finalPrice - (finalPrice * totalDiscount) / 100;
   } else {
     const cartPrice = cart.items.reduce((total, item) => {
-      if(item.discountApplied.length === 0){
-        return total + item.quantity * item.price
+      if (item.discountApplied.length === 0) {
+        return total + item.quantity * item.price;
       } else {
-        return total
+        return total;
       }
     }, 0);
     totalPrice = cartPrice + totalDiscountedItemPrice;
-    
   }
   return totalPrice;
 };
 
-
-
 // show all cart items
 
 const getCartItems = (): CartItem[] => {
-  return cart.items.filter((product) => product.quantity >= 1);
+  const cartItems = cart.items.filter(
+    (product, index) =>
+      product.quantity >= 1 &&
+      cart.items.findIndex((item) => item.id === product.id) === index
+  );
+  return cartItems;
 };
 
-
-export { addItem, removeItem, addDiscount, getCartItemPrice, getTotalPrice, getCartItems, cart, discountCodes };
+export {
+  addItem,
+  removeItem,
+  addDiscount,
+  getCartItemPrice,
+  getTotalPrice,
+  getCartItems,
+  cart,
+  discountCodes,
+};
